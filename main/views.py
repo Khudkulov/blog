a884290b976b386eb42ebe507affcc7911d17005
Khudoykulov <diyorbekxudoyqulov627@gmail.com>
2024-01-26 19:23:46 +0500

    2

--- main/views.py
+++ main/views.py
@@ -6,19 +6,26 @@ from django.contrib import messages
 
 def home_view(request):
     articles = Article.objects.all()
-    # add = []
-    # for article in articles:
-    #     # articles = Article.objects.order_by('-id').filter(comments=)
-    #     comment_piece = article.comments.count()
-    #     add.append(comment_piece)
-    # add.sort()
-    #
-    # print(add)
+
+    add = []
+    for article in articles:
+        comment_piece = article.comments.count()
+        add.append(comment_piece)
+        add.sort()
+        add.reverse()
+        print(add)
+    max_articles = []
+    for i in add[:3]:
+        for article in articles:
+            if article.comments.count() == i:
+                max_articles.append(article)
+    articles = max_articles
     categories = Category.objects.all()
     tags = Tag.objects.all()
     articles_2 = Article.objects.order_by('-id')[3:]
     import random
     article_random = Article.objects.order_by('id')[random.randint(0, len(Article.objects.order_by('id'))-1)]
+
     context = {
         'object_list': articles,
         'categories': categories,
